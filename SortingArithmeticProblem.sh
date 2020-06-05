02a503b6cff040af034e8c2bb5bf0801d047322f
TanmayeePatil <tanmayeepatil@outlook.com>
2020-06-06 04:34:20 +0530

    Added: Accept Input functionality in problem

--- SortingArithmeticProblem.sh
+++ SortingArithmeticProblem.sh
@@ -2,4 +2,12 @@
 
 echo "Welcome to Sorting Arithmetic Operation Problem:"
 
+read -p "Enter value of a: " a
+read -p "Enter value of b: " b
+read -p "Enter value of c: " c
+
+echo "You given below inputes"
+echo "a=$a"
+echo "b=$b"
+echo "c=$c"
 
