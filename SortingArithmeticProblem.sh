a6e8e68c9b6f3dda693e9534a6dcb9517675a5b1
TanmayeePatil <tanmayeepatil@outlook.com>
2020-06-06 04:44:41 +0530

    Added: Compute a+b*c operation in problem

--- SortingArithmeticProblem.sh
+++ SortingArithmeticProblem.sh
@@ -2,6 +2,8 @@
 
 echo "Welcome to Sorting Arithmetic Operation Problem:"
 
+echo "USE CASE 1:"
+
 read -p "Enter value of a: " a
 read -p "Enter value of b: " b
 read -p "Enter value of c: " c
@@ -11,3 +13,7 @@ echo "a=$a"
 echo "b=$b"
 echo "c=$c"
 
+echo -e "\nUSE CASE 2:"
+
+uc2Result=$(( a+b*c ))
+echo "a+b*c = $uc2Result"
